ec3ae58e8028de5956a7f5ba65f81397ab181810
Len Chan <mail@lenchan139.org>
2017-09-18 21:04:45 +0800

    bug fix

--- app/src/main/java/org/lenchan139/littleshortcutter/MainActivity.kt
+++ app/src/main/java/org/lenchan139/littleshortcutter/MainActivity.kt
@@ -16,6 +16,11 @@ import android.os.Build
 import android.app.NotificationManager
 import android.content.pm.PackageManager
 import android.support.v4.app.ActivityCompat
+import android.view.WindowManager
+import android.app.KeyguardManager.KeyguardLock
+import android.app.KeyguardManager
+
+
 
 
 class MainActivity : Activity() {
@@ -25,7 +30,7 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main);
-
+        bypass()
         dialog = AlertDialog.Builder(this)
         dialog.setItems(arrDialogItem, DialogInterface.OnClickListener { dialogInterface, i ->
            if (i==0){
@@ -46,6 +51,21 @@ class MainActivity : Activity() {
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+        bypass()
+    }
+    fun bypass(){
+        /******block is needed to raise the application if the lock is*********/
+        var wind = this.getWindow();
+        wind.addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+        wind.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+        wind.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        /* ^^^^^^^block is needed to raise the application if the lock is*/
+        val manager = this.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+        val lock = manager.newKeyguardLock("abc")
+        lock.disableKeyguard()
+    }
     override fun onPause() {
         super.onPause()
         overridePendingTransition(0, 0)
@@ -81,6 +101,10 @@ class MainActivity : Activity() {
         am = baseContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
         if(am.ringerMode == AudioManager.RINGER_MODE_NORMAL) {
             am.ringerMode = AudioManager.RINGER_MODE_SILENT
+            val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mNotificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_NONE)
+            }
         }else if(am.ringerMode == AudioManager.RINGER_MODE_SILENT){
             am.ringerMode = AudioManager.RINGER_MODE_NORMAL
         }else{
